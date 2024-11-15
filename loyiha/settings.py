c6cf03638c6df7f6f7b348bb6054773b491fa127
shakhrillodev <shaxrillonusratov@gmail.com>
2024-11-15 11:57:45 +0500

    updates

--- loyiha/settings.py
+++ loyiha/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$o#er^&qhp=tv@46t5^*4*52mn*fm)@=svvn#r)5b*n%!b+8+k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +121,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+STATIC_ROOT=BASE_DIR/"staticfiles"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
 # Default primary key field type
