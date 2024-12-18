1067fca237b9342381e815db764f0c178a6fa195
shakhrillodev <shaxrillonusratov@gmail.com>
2024-12-18 12:27:17 +0500

    [main] add actions

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import include, path
-from .views import NewsListView
+from .views import NewsListView, DetailView, UpdateView, DeleteView, ActionView
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
+    path('<int:pk>/', DetailView.as_view(), name="news-detail"),
+    path('<int:pk>/update/', UpdateView.as_view(), name="news-update"),
+    path('<int:pk>/delete/', DeleteView.as_view(), name="news-delete"),
+    path('<int:pk>/action/', ActionView.as_view(), name="news-action"),
     path('', NewsListView.as_view(), name="newslist"),
 ]
--- api/views.py
+++ api/views.py
@@ -2,10 +2,27 @@ from django.shortcuts import render
 from.serializers import NewsSerializer
 from app.models import News
 from rest_framework.views import APIView, Response
+from rest_framework.generics import RetrieveAPIView, UpdateAPIView, DestroyAPIView, RetrieveUpdateDestroyAPIView
 # Create your views here.
 
 class NewsListView(APIView):
     def get(self, response):
         posts=News.objects.all()
-        tests_serializer=NewsSerializer (posts, many=True)
+        tests_serializer=NewsSerializer(posts, many=True)
         return Response(tests_serializer.data)
+    
+class DetailView(RetrieveAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+
+class UpdateView (UpdateAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+
+class DeleteView (DestroyAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
+
+class ActionView(RetrieveUpdateDestroyAPIView):
+    queryset = News.objects.all()
+    serializer_class = NewsSerializer
